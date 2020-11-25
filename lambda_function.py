065fc20952f9e5a611c36e4eb8fefd81c732cd21
Craig Hamilton <349492+cch71@users.noreply.github.com>
2020-11-25 11:22:26 -0600

    Adding query specific functionality

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,50 @@
 import json
 import boto3
+from decimal import Decimal
+from boto3.dynamodb.conditions import Key
+
+def json_default_encoder(obj):
+     if isinstance(obj, Decimal):
+        if obj % 1 > 0:
+           return float(obj)
+        else:
+            return int(obj)
+     raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('Orders')
-    response = table.scan()
-    items = response['Items']
-    print(items)
+    table = dynamodb.Table('T27FundraiserOrders')
+
+
+    req = event['body']
+    if isinstance(req, str):
+        req = json.loads(req)
+    
+    retvals=[]
+    if 'orderOwner' in req:
+        # Expression attribute names can only reference items in the projection expression.
+        #ProjectionExpression=", ".join(req['fields']),
+        query_args = {}
+        if 'orderId' in req:
+            query_args['KeyConditionExpression'] = Key('orderOwner').eq(req['orderOwner']) & Key('orderId').eq(req['orderId'])
+        else:
+            query_args['KeyConditionExpression'] = Key('orderOwner').eq(req['orderOwner'])
+
+        if 'fields' in req:
+            query_args['ProjectionExpression'] = ", ".join(req['fields'])
+
+        response = table.query(**query_args)
+                
+        retvals=response['Items']
+
+    retvals = json.dumps(retvals, default=json_default_encoder)
+    print(f"Query Resp: {retvals}")
 
     return {
         'statusCode': 200,
-        'body': json.dumps({'items': items}),
+        'body': retvals,
         'headers': {
             'Access-Control-Allow-Origin': '*',
         },
     }
+    
